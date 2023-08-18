7eb316b112364676ae33d2f8b69b07c800a54a22
Arun Chithambaram <arun.chithambaram@newglobe.education>
2023-08-19 00:56:59 +0530

    Implements mouse interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { ThemeProvider, styled } from 'styled-components';
 import { Theme, GlobalStyle } from './components/Theme';
 import { Button, Divider, Header, Nav } from './components';
-
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import { SetStateAction, useState } from 'react';
 const Container = styled.div`
     margin: 0.5rem;
     margin-bottom: unset;
@@ -18,6 +19,30 @@ const SubContainer = styled.div`
 `;
 
 const App = () => {
+    const [points, setPoints] = useState<Array<{ x: number; y: number }>>([]);
+    const [zoomLevel, setZoomLevel] = useState(1);
+    const [isPanning, setIsPanning] = useState(false);
+    const [maskOpacity, setMaskOpacity] = useState(0);
+
+    const handleClick = (event: React.MouseEvent) => {
+        if (isPanning) {
+            return;
+        }
+        const rect = (event.target as HTMLDivElement).getBoundingClientRect();
+
+        const x = (event.clientX - rect.left) / zoomLevel;
+        const y = (event.clientY - rect.top) / zoomLevel;
+        const newPoint = { x, y };
+
+        setPoints((prevPoints) => [...prevPoints, newPoint]);
+    };
+
+    const handleZoom = (newZoomLevel: {
+        state: { scale: SetStateAction<number> };
+    }) => {
+        setZoomLevel(newZoomLevel.state.scale);
+    };
+
     return (
         <ThemeProvider theme={Theme}>
             <GlobalStyle />
@@ -43,18 +68,83 @@ const App = () => {
                     >
                         Submit Feedback
                     </Button>
+                    <input
+                        type='range'
+                        onChange={(e) =>
+                            setMaskOpacity(Number(e.target.value) / 100)
+                        }
+                    />
                 </Nav>
 
                 <Divider variant='vertical' />
 
                 <SubContainer>
-                    <Button
-                        variant='text'
-                        icon={{ location: 'left', src: 'feedback' }}
-                        to='http://www.duckduckgo.com'
+                    <div
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
                     >
-                        Submit Feedback
-                    </Button>
+                        <TransformWrapper
+                            initialScale={1}
+                            centerOnInit={true}
+                            onZoom={handleZoom}
+                            onPanning={() => setIsPanning(true)}
+                            onPanningStop={() =>
+                                setTimeout(() => {
+                                    setIsPanning(false);
+                                }, 1)
+                            }
+                        >
+                            <TransformComponent>
+                                <div
+                                    style={{ position: 'relative' }}
+                                    onClick={(e) => handleClick(e)}
+                                >
+                                    <img
+                                        src='/obj.jpeg'
+                                        alt='test'
+                                        style={{
+                                            maxHeight: '30rem',
+                                        }}
+                                    />
+                                    <div
+                                        style={{
+                                            position: 'absolute',
+                                            top: 0,
+                                            left: 0,
+                                            width: '100%',
+                                            height: '100%',
+                                            zIndex: 1,
+                                            backgroundImage: `url('/obj_mask.jpeg')`,
+                                            backgroundSize: 'contain',
+                                            opacity: maskOpacity,
+                                        }}
+                                    ></div>
+                                    {points.map((point, index) => (
+                                        <div
+                                            key={index}
+                                            style={{
+                                                position: 'absolute',
+                                                left: point.x,
+                                                top: point.y,
+                                                width: 10,
+                                                height: 10,
+                                                borderRadius: '50%',
+                                                background: 'red',
+                                                scale: `${1.25 / zoomLevel}`,
+                                                translate: '-50%  -50%',
+                                            }}
+                                        ></div>
+                                    ))}
+                                </div>
+                            </TransformComponent>
+                        </TransformWrapper>
+                    </div>
+
                     <Divider variant='horizontal' />
                 </SubContainer>
             </Container>
