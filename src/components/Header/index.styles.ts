2432231a295944aa28e94df1bad4d11c25e452cc
Arun Chithambaram <arun.chithambaram@newglobe.education>
2023-08-19 17:01:31 +0530

    Updates header styles

--- src/components/Header/index.styles.ts
+++ src/components/Header/index.styles.ts
@@ -6,7 +6,6 @@ export const HeaderContainer = styled.header`
     padding: 0.9375rem 1.25rem 0.9375rem 1.375rem;
     backdrop-filter: blur(2.1875rem);
     background-color: hsla(0, 0%, 100%, 0.6);
-    border: 0.0625rem solid white;
     border-radius: 0.625rem;
 
     display: flex;
