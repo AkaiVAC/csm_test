192f4673592163923583fd86439c972f0d8e8edf
Arun Chithambaram <arun.chithambaram@newglobe.education>
2023-08-20 20:23:26 +0530

    Adds tooltips to side nav icons

--- src/components/Nav/SideNav.tsx
+++ src/components/Nav/SideNav.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from 'react-tooltip';
 import Nav from '.';
 import { Button, Divider } from '..';
 
@@ -5,18 +6,33 @@ const SideNav = () => {
     return (
         <>
             <Nav variant='vertical'>
+                <Tooltip
+                    anchorSelect='.overview'
+                    content='Overview'
+                    place='right'
+                />
                 <Button
                     variant='sidebar'
                     icon={{ location: 'left', src: 'overview' }}
                 >
                     Overview
                 </Button>
+                <Tooltip
+                    anchorSelect='.segmentVideo'
+                    content='Video segmentation'
+                    place='right'
+                />
                 <Button
                     variant='sidebar'
                     icon={{ location: 'left', src: 'segmentVideo' }}
                 >
                     Video Segmentation
                 </Button>
+                <Tooltip
+                    anchorSelect='.feedback'
+                    content='Go to feedback page'
+                    place='right'
+                />
                 <Button
                     variant='sidebar'
                     icon={{ location: 'left', src: 'feedback' }}
